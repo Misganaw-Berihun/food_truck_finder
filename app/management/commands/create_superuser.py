36192b693e1285a08a5974a137b4875fa37db21d
Misganaw-Berihun <misganawbmb@gmail.com>
2024-06-10 12:07:02 +0300

    [FIX]: add docstring

--- app/management/commands/create_superuser.py
+++ app/management/commands/create_superuser.py
@@ -2,10 +2,28 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 import os
 
+
 class Command(BaseCommand):
+    """
+    Django management command to create a superuser if one does
+    not already exist.
+
+    Attributes:
+        help (str): A brief description of what the command does.
+
+    Methods:
+        handle(*args, **options):
+            Executes the command logic to check for the existence of
+            the superuser and create one if necessary.
+    """
     help = 'Create a superuser if not exists'
 
     def handle(self, *args, **options):
+        """
+        Executes the command to create a superuser if one does not
+        already exist.
+
+        """
         User = get_user_model()
         if not User.objects.filter(
                 username=os.getenv('DJANGO_SUPERUSER_USERNAME')
